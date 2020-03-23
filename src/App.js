98ae0528973a440622dc022c12f4fed777c1b6fa
Jonno <jonnowelch@yahoo.com>
2020-03-23 19:10:18 +0000

    added new pages, learning about contexts. stopped as need to figure out why console log happening twice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import ItemsList from "./components/ItemsList";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <ItemsList />
+      {console.log("hello")}
+      <ThemeContextProvider>
+        <Navbar />
+        <ItemsList />
+      </ThemeContextProvider>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends Component {
+  static contextType = ThemeContext;
   render() {
+    console.log(this.context);
     return (
       <nav>
         <h1> Todo App</h1>
