613631e8691b19901d290234334a91ffc25bcf82
Jonno <jonnowelch@yahoo.com>
2020-03-23 12:51:18 +0000

    first commit

---
+++ src/components/ItemsList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class componentName extends Component {
+  render() {
+    return (
+      <div className="items-list">
+        <ul>
+          <li>milk</li>
+          <li>eggs</li>
+          <li>butter</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default componentName;
